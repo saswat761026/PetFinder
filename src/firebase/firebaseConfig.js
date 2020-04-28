4b14c01509bb4711df26e7dc558fac0fcd9b7dd2
Saswat Panda <saswatpanda104@gmail.com>
2020-04-28 12:34:22 +0530

    commiting changes

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,8 @@
 import firebase from "firebase";
 
+const admin = require('firebase-admin'); 
+let serviceAccount = require('./flightmanagementsystem-b087d-7affb996188c')
+
 // Your web app's Firebase configuration
 const FirebaseConfig = {
   apiKey: "AIzaSyBt2yU_IHuSyxsAr3S8gTtF4RgOev7xRCw",
@@ -10,9 +13,12 @@ const FirebaseConfig = {
   messagingSenderId: "421159042782",
   appId: "1:421159042782:web:0902112ff48f92a3a10c4f",
   measurementId: "G-8FVTDCCHNN",
+  credential: admin.credential.cert(serviceAccount)
+  
 };
 // Initialize Firebase
 firebase.initializeApp(FirebaseConfig);
 firebase.analytics();
 
 export const auth = firebase.auth();
+export const db = admin.firestore();
